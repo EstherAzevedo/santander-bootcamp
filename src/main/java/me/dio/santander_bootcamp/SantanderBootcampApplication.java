ec3749c17a26759aef1f12dd6075283d95863562
EstherAzevedo <estherazvdo123@gmail.com>
2024-06-20 15:11:49 -0300

    second commit

--- src/main/java/me/dio/santander_bootcamp/SantanderBootcampApplication.java
+++ src/main/java/me/dio/santander_bootcamp/SantanderBootcampApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SantanderBootcampApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SantanderBootcampApplication.class, args);
 	}
 
